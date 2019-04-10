e38c825ef3b591f63abd2258714aab01e7f49f3b
Diogo Cezar <diogo@diogocezar.com>
2019-04-10 18:18:09 -0300

    Static Files

--- back-end/src/models/File.js
+++ back-end/src/models/File.js
@@ -13,7 +13,13 @@ const File = new mongoose.Schema(
   },
   {
     timestamps: true,
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true },
   },
 )
+
+File.virtual('url').get(function () {
+  return `http://localhost:3333/files/${encodeURIComponent(this.path)}`
+})
 
 module.exports = mongoose.model('File', File)
--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
 const routes = require('./routes')
 
 const app = express()
@@ -8,6 +9,7 @@ mongoose.connect('mongodb://localhost:27017/oministack', { useNewUrlParser: true
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')))
 
 app.use(routes)
 
