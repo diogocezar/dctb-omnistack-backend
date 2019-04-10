7e15a30328ba996f0d715ed01d629f1a9bb727ad
Diogo Cezar <diogo@diogocezar.com>
2019-04-10 17:26:37 -0300

    Routes Structus and nodemoon

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,1 +1,11 @@
 const express = require('express')
+const routes = require('./routes')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(routes)
+
+app.listen(3333)
