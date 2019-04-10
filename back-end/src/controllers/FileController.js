e747616245d75ac98e9fc8e490aae90dc49e085c
Diogo Cezar <diogo@diogocezar.com>
2019-04-10 18:26:20 -0300

    General Adjustments

--- back-end/src/controllers/FileController.js
+++ back-end/src/controllers/FileController.js
@@ -7,6 +7,7 @@ class FileController {
     const file = await File.create({ title: req.file.originalname, path: req.file.key })
     box.files.push(file)
     await box.save()
+    req.io.sockets.in(box._id).emit('file', file)
     return res.json(file)
   }
 }
--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,16 +1,30 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
-const routes = require('./routes')
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const routes = require('./routes')
+
+io.on('connection', (socket) => {
+  socket.on('connectRoom', (box) => {
+    socket.join(box)
+  })
+})
 
 mongoose.connect('mongodb://localhost:27017/oministack', { useNewUrlParser: true })
 
+app.use((req, res, next) => {
+  req.io = io
+  return next()
+})
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')))
 
 app.use(routes)
 
-app.listen(3333)
+server.listen(3333)
